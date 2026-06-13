cda8642da00d3c54e02e0b758476957a736a445e
dean6609 <monoazul148@gmail.com>
2026-06-12 22:30:51 -0500

    chore(ui): remove obsolete Good-Fella components, effects and hooks

--- frontend/src/components/SmoothScroll.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useLenis } from "../hooks/useLenis";
-
-export function SmoothScroll({ children }: { children: React.ReactNode }) {
-  useLenis();
-  return <>{children}</>;
-}
